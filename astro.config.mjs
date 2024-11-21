9706d8bcfc3141a5bb0e8624a786e8e3c8c23cb2
holaleche <whoisleche@gmail.com>
2024-11-21 23:28:11 +0900

    Deploy netlify 배포 주소 반영

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ const LIVE_URL = "https://yourwebsiteurl.com";
 // this is the astro command your npm script runs
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
-let BASE_URL = LOCALHOST_URL;
+let BASE_URL = "https://devrey.blog";
 // When you're building your site in local or in CI, you could just set your URL manually
 if (isBuild) {
   BASE_URL = LIVE_URL;
