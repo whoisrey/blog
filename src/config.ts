d54661d6f66f5ad7db7d25acf51b7837ccad26e1
holaleche <whoisleche@gmail.com>
2025-02-16 18:00:00 +0900

    Update 멘션 이름

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@
 
 export const SITE_TITLE = "기록하고 기억하기";
 export const SITE_DESCRIPTION = "개발자 이양래의 기록.";
-export const TWITTER_HANDLE = "@yourtwitterhandle";
+export const TWITTER_HANDLE = "@DevBlog";
 export const MY_NAME = "Rey";
 
 // setup in astro.config.mjs
